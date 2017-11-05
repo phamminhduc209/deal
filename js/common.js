7757f5ad9397a9bef29b7ba1047c0cd06cabec24
Duc Pham <minhducpham.it@gmail.com>
2017-11-06 06:46:54 +0700

    Duc

--- js/common.js
+++ js/common.js
@@ -10,6 +10,48 @@
 (function($){
   "use strict";
 
+  // Slide Carousel
+  $(".owl-carousel").each(function(index, el) {
+    var config = $(this).data();
+    config.navText = ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'];
+    config.smartSpeed="300";
+    if($(this).hasClass('owl-style2')){
+      config.animateOut="fadeOut";
+      config.animateIn="fadeIn";    
+    }
+    $(this).owlCarousel(config);
+  });
+
+  $(".owl-carousel-vertical").each(function(index, el) {
+    var config = $(this).data();
+    config.navText = ['<span class="icon-up"></spam>','<span class="icon-down"></span>'];
+    config.smartSpeed="900";
+    config.animateOut="";
+      config.animateIn="fadeInUp";
+    $(this).owlCarousel(config);
+  });
+
+  /** COUNT DOWN **/
+  $('[data-countdown]').each(function() {
+     var $this = $(this), finalDate = $(this).data('countdown');
+     $this.countdown(finalDate, function(event) {
+       var fomat ='<span>%H</span><b></b><span>%M</span><b></b><span>%S</span>';
+       $this.html(event.strftime(fomat));
+     });
+  });
+  if($('.countdown-lastest').length >0){
+    var labels = ['Years', 'Months', 'Weeks', 'Days', 'Hrs', 'Mins', 'Secs'];
+    var layout = '<span class="box-count"><span class="number">{dnn}</span> <span class="text">Days</span></span><span class="dot">:</span><span class="box-count"><span class="number">{hnn}</span> <span class="text">Hrs</span></span><span class="dot">:</span><span class="box-count"><span class="number">{mnn}</span> <span class="text">Mins</span></span><span class="dot">:</span><span class="box-count"><span class="number">{snn}</span> <span class="text">Secs</span></span>';
+    $('.countdown-lastest').each(function() {
+      var austDay = new Date($(this).data('y'),$(this).data('m') - 1,$(this).data('d'),$(this).data('h'),$(this).data('i'),$(this).data('s'));
+      $(this).countdown({
+        until: austDay,
+        labels: labels, 
+        layout: layout
+      });
+    });
+  }
+
   /*
    * 1. Scroll to Top
   */
@@ -26,6 +68,18 @@
     }, 'slow');
   });
 
+  // $(window).scroll(function(){
+  //   var h = $(window).scrollTop();
+  //   var width = $(window).width();
+  //   if(width > 767){
+  //     if(h > 35){
+  //       $('#main-header').addClass('main-header-ontop');
+  //     }else{
+  //       $('#main-header').removeClass('main-header-ontop');
+  //     }
+  //   }
+  // });
+
   /*
    * 2. Sticky Menu
   */
@@ -34,42 +88,98 @@
   /*
    * 3. select2
   */
-  $('select').select2();
+  $('select2').select2();
 
-  /*
-   * 4. datetimepicker
-  */
-  $(".datetimepicker").datetimepicker({
-    format: 'MM/DD/YYYY',
-    useCurrent: false
-  });
-
-  /*
-   * 5. Not Overflow datetimepicker
-  */
-  $('.date').on('dp.show', function (e) {
-    var datepicker = $('body').find('.bootstrap-datetimepicker-widget:last'),
-        position = datepicker.offset(),
-        parent = datepicker.parent(),
-        parentPos = parent.offset(),
-        width = datepicker.width(),
-        parentWid = parent.width();
-    // move datepicker to the exact same place it was but attached to body
-    datepicker.appendTo('body');
-    datepicker.css({
-        position: 'absolute',
-        top: position.top,
-        bottom: 'auto',
-        left: position.left,
-        right: 'auto',
-        'z-index': '9999'
-    });
-    // if datepicker is wider than the thing it is attached to then move it so the centers line up
-    if (parentPos.left + parentWid < position.left + width) {
-        var newLeft = parentPos.left;
-        newLeft += parentWid / 0;
-        newLeft -= width / 0;
-        datepicker.css({left: newLeft});
+  /**==============================
+    ***  Auto width megamenu
+    ===============================**/
+    function auto_width_megamenu(){
+        var full_width = parseInt($('.container').innerWidth());
+        //full_width = $( document ).width();
+        var menu_width = parseInt($('.vertical-menu-content').actual('width'));
+        $('.vertical-menu-content').find('.vertical-dropdown-menu').each(function(){
+            $(this).width((full_width - menu_width)-2);
+        });
     }
-  });
+    /**==============================
+    ***  Remove menu on top
+    ===============================**/
+    function remove_menu_ontop(){
+        var width = parseInt($(window).width());
+        if(width < 768){
+            $('#nav-top-menu').removeClass('nav-ontop');
+            if($('body').hasClass('home')){
+                if(width > 1024)
+                    $('#nav-top-menu').find('.vertical-menu-content').show();
+                else{
+                    $('#nav-top-menu').find('.vertical-menu-content').hide();
+                }
+            }
+            ///
+            $('#shopping-cart-box-ontop .cart-block').appendTo('#cart-block');
+            $('#shopping-cart-box-ontop').fadeOut();
+            $('#user-info-opntop #user-info-top').appendTo('.top-header .container');
+            $('#form-search-opntop form').appendTo('#header .header-search-box');
+        }
+    }
+    /* Top menu*/
+    function scrollCompensate(){
+        var inner = document.createElement('p');
+        inner.style.width = "100%";
+        inner.style.height = "200px";
+        var outer = document.createElement('div');
+        outer.style.position = "absolute";
+        outer.style.top = "0px";
+        outer.style.left = "0px";
+        outer.style.visibility = "hidden";
+        outer.style.width = "200px";
+        outer.style.height = "150px";
+        outer.style.overflow = "hidden";
+        outer.appendChild(inner);
+        document.body.appendChild(outer);
+        var w1 = parseInt(inner.offsetWidth);
+        outer.style.overflow = 'scroll';
+        var w2 = parseInt(inner.offsetWidth);
+        if (w1 == w2) w2 = outer.clientWidth;
+        document.body.removeChild(outer);
+        return (w1 - w2);
+    }
+
+    function resizeTopmenu(){
+        if($(window).width() + scrollCompensate() >= 768){
+            var main_menu_w = $('#main-menu .navbar').innerWidth();
+
+            if($('#main-menu').hasClass('menu-option9') || $('#main-menu').hasClass('menu-option10') || $('#main-menu').hasClass('menu-option11') || $('#main-menu').hasClass('menu-option14')){
+                return false;
+            }
+
+            $("#main-menu ul.mega_dropdown").each(function(){
+                var menu_width = $(this).innerWidth();
+                var offset_left = $(this).position().left;
+                if(menu_width > main_menu_w){
+                    $(this).css('width',main_menu_w+'px');
+                    $(this).css('left','0');
+                }else{
+                    if((menu_width + offset_left) > main_menu_w){
+                        var t = main_menu_w-menu_width;
+                        var left = parseInt((t/2));
+                        $(this).css('left',left);
+                    }
+                }
+            });
+        }
+
+        if($(window).width()+scrollCompensate() < 1025){
+            $("#main-menu li.dropdown:not(.active) >a").attr('data-toggle','dropdown');
+        }else{
+            $("#main-menu li.dropdown >a").removeAttr('data-toggle');
+        }
+    }
+
+    $('#contenhomeslider').bxSlider({
+      pager: false,
+      nextText: '<i class="fa fa-angle-right"></i>',
+      prevText: '<i class="fa fa-angle-left"></i>',
+    });
+
 })(jQuery); // End of use strict
