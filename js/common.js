9b754f960e2ca142ca4576eb949e0279905ec50c
HoangNH <mrhoang1989@gmail.com>
2017-11-16 13:55:05 +0700

    ,

--- js/common.js
+++ js/common.js
@@ -211,6 +211,13 @@ window.fbAsyncInit = function() {
     version    : 'v2.7' // use graph api version 2.7
   });
 };
+ (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 $(document).ready(function() {
   $('.login-by-facebook-popup').click(function() {
     FB.login(function(response){
